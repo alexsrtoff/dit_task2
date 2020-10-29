fdf45e3131817704a69f887c81d5a33e895c8b9c
alexsrtoff <alexei.strelnikov@gmail.com>
2020-10-29 16:49:52 +0300

    DIT task2

--- src/main/java/ru/sber/FileWatchService.java
+++ src/main/java/ru/sber/FileWatchService.java
@@ -5,6 +5,7 @@ import ru.sber.service.*;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.List;
 
 @Slf4j
 public class FileWatchService {
@@ -28,7 +29,6 @@ public class FileWatchService {
 
             WatchKey key;
             while ((key = watchService.take()) != null) {
-
                 for (final WatchEvent<?> event : key.pollEvents()) {
                     if (event.kind().name().equals("ENTRY_CREATE")) {
                         log.info("Add new file {}, file create at: {}", event.context(), Utils.formatFileTime(filePath, event.context().toString()));
@@ -47,6 +47,7 @@ public class FileWatchService {
                         }
                     }).start();
                 }
+                key.reset();
             }
         } catch (InterruptedException | IOException e) {
             log.error("Something went wrong ", e);
